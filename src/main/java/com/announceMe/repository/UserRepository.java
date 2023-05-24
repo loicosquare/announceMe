95f1d86bb9519fb9d4da764700b695eab94c8184
Loic Sanou <loicsan07@gmail.com>
2023-05-24 14:13:36 +0200

    Addind FindByEmail on the repository

--- src/main/java/com/announceMe/repository/UserRepository.java
+++ src/main/java/com/announceMe/repository/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     /*public List<User> findAllUsers();
     void deleteUserById(Long id);*/
+    Optional<User> findByEmail(String email);
 }
