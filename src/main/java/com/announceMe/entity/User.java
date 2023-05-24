7b61cc227c272baceb9e2576800ba90095d6e934
Loic Sanou <loicsan07@gmail.com>
2023-05-24 12:01:55 +0200

    Addind additionnal fields for UserDetails Class and overriding the incoming methods from it

--- src/main/java/com/announceMe/entity/User.java
+++ src/main/java/com/announceMe/entity/User.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.mapping.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -16,17 +19,26 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id")
     private Long id;
 
-    @Column(name="username")
-    private String username;
+    @Column(name="firstname")
+    private String firstname;
 
-    @Column(name="login")
-    private String login;
+    @Column(name="lastname")
+    private String lastname;
+
+    /*@Column(name="username")
+    private String username;*/
+
+    @Column(name="email")
+    private String email;
+
+    /*@Column(name="login")
+    private String login;*/
 
     @Column(name="password")
     private String password;
@@ -39,4 +51,42 @@ public class User {
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Comment> comments;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { //This method return a list of roles, this is why we need to create a list of roles on the user class
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; //I can return another thing.
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
